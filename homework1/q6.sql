1c5e0d690e2f9f9d7526ffffdb3039e2a38602c9
chaser-gua <112944897+chaser-gua@users.noreply.github.com>
2022-10-21 12:53:03 +0800

    Update q6.sql

--- homework1/q6.sql
+++ homework1/q6.sql
@@ -5,20 +5,3 @@ from `order`
 where `customerid` == 'BLONP'
 order by `orderdate`
 limit 10)
-
-
-
--- SELECT
---      Id
---      , OrderDate
---      , PrevOrderDate
---      , ROUND(julianday(OrderDate) - julianday(PrevOrderDate), 2)
--- FROM (
---      SELECT Id
---           , OrderDate
---           , LAG(OrderDate, 1, OrderDate) OVER (ORDER BY OrderDate ASC) AS PrevOrderDate
---      FROM 'Order' 
---      WHERE CustomerId = 'BLONP'
---      ORDER BY OrderDate ASC
---      LIMIT 10
--- );
